5feb88f94c08ca32066dd496961aa00e7d8203e2
hamzakebir <hamzux200@gmail.com>
2024-03-02 21:50:25 +0100

    Mofification statistique css

--- src/components/statistique.js
+++ src/components/statistique.js
@@ -6,12 +6,8 @@ export default function Statistique() {
 
     return (
         <div className='container'>
-            <div className="grid grid-cols-1 lg:grid-cols-3 gap-3" style={{ 
-                marginLeft: '50px',
-             }}>
-                <div className="lg:col-span-1" style={{ 
-                    marginTop: '20px',
-                }}>
+            <div className="statistics-content grid grid-cols-1 lg:grid-cols-3 gap-3">
+                <div className="lg:col-span-1 first">
                     <div className="grid grid-cols-3 gap-4">
                         <div className="image-container-certificate">
                             <StaticImage
