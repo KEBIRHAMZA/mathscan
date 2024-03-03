f47ca74ed6abfd030d615e62fed15a9055042496
hamzakebir <hamzux200@gmail.com>
2024-03-03 16:59:35 +0100

    Modification Ecole composant

--- src/components/astuces.js
+++ src/components/astuces.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import { StaticImage } from 'gatsby-plugin-image'
-import '../styles/ecole.css'
+import '../styles/astuces.css'
 
 export default function Astuces() {
   return (
     <>
-    <section style={{ marginTop:'-200px' }} className="mb-40 conatiner-for-school">
+    <section style={{ marginTop:'-200px' }} className="mb-40 conatiner-for-astuces">
         <div className="px-6 py-12 text-center dark:bg-neutral-900 md:px-12 lg:text-left">
         <div className="w-100 mx-auto sm:max-w-2xl md:max-w-3xl lg:max-w-5xl xl:max-w-7xl">
             <div className="grid items-center gap-12 lg:grid-cols-2">
@@ -13,19 +13,19 @@ export default function Astuces() {
                 <StaticImage
                     src="../images/ebook.png"
                     alt="Flowbite Logo"
-                    className='image-for-school'
+                    className='image-for-astuces'
                 />
             </div>
             <div className="mt-12 lg:mt-0">
-                <p className='titre-two-school'>
+                <p className='titre-two-astuces'>
                     5 Astuces pour aider votre enfant à exceller en math
                 </p>
-                <p className='text-school'>
+                <p className='text-astuces'>
                     Un ebook gratuit pour les parents et les enfants qui souhaitent savoir quelques astuces.
                 </p>
                 <a
                     href="#"
-                    className="btn-school"
+                    className="btn-astuces"
                     style={{ 
                         backgroundColor: '#0dcaf0'
                      }}
