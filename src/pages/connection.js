435d407bc6483b0d6fa5c7f9ed8c6c2957b2bf3c
hamzakebir <hamzux200@gmail.com>
2024-03-06 16:41:31 +0100

    Page inscription

--- src/pages/connection.js
+++ src/pages/connection.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 import { useIntl } from 'gatsby-plugin-intl';
+import { StaticImage } from 'gatsby-plugin-image';
 
 export default function Connection() {
 
@@ -94,9 +95,9 @@ export default function Connection() {
             </form>
             </div>
             <div className="relative h-64 w-full sm:h-96 lg:h-full lg:w-1/2">
-            <img
+            <StaticImage
                 alt=""
-                src="https://images.unsplash.com/photo-1630450202872-e0829c9d6172?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80"
+                src="../images/accueil-images/header.png"
                 className="absolute inset-0 h-full w-full object-cover"
             />
             </div>
