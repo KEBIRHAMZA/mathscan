fbd8fc8a3c57772f9c670c2a3117e39676a69f35
hamzakebir <hamzux200@gmail.com>
2024-03-09 15:50:05 +0100

    Readme file

--- src/pages/inscription.js
+++ src/pages/inscription.js
@@ -14,7 +14,7 @@ export default function Inscription() {
 
   return (
     <Layout>
-        <section className="relative flex flex-wrap lg:h-screen lg:items-center">
+        <section style={{ marginBottom : "10%" }} className="relative flex flex-wrap lg:h-screen lg:items-center">
             <div className="w-full px-4 py-12 sm:px-6 sm:py-16 lg:w-1/2 lg:px-8 lg:py-24">
             <div className="mx-auto max-w-lg text-center">
                 <h1 className="text-2xl font-bold sm:text-3xl">{intl.formatMessage({ id: "inscription" })}</h1>
