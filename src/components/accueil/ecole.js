40d135a03c9fd7e768efa9a79051a74b55e2e602
hamzakebir <hamzux200@gmail.com>
2024-03-09 02:27:25 +0100

    fix respossive design

--- src/components/accueil/ecole.js
+++ src/components/accueil/ecole.js
@@ -7,23 +7,26 @@ import { Link } from 'gatsby';
 export default function Ecole() {
   const intl = useIntl();
 
+  const locale = intl.locale;
+  const direction = locale === 'ar' ? 'rtl' : 'ltr';
+
   return (
     <>
     <section className="mb-40 conatiner-for-school">
         <div className="px-6 py-12 text-center dark:bg-neutral-900 md:px-12 lg:text-left">
         <div className="w-100 mx-auto sm:max-w-2xl md:max-w-3xl lg:max-w-5xl xl:max-w-7xl">
             <div className="grid items-center gap-12 lg:grid-cols-2">
-            <div className="mt-12 lg:mt-0">
+            <div dir='ltr' className="mt-12 lg:mt-0">
                 <h1 className='titre-for-school'>
                     {intl.formatMessage({ id: "schoolSectionTitle" })}
                 </h1>
-                <p className='titre-two-school'>
+                <p  dir={direction} className='titre-two-school'>
                     {intl.formatMessage({ id: "schoolSectionSubtitle" })} <span style={{ color : '#FF64B1' }}>{intl.formatMessage({ id: "schoolSectionHours" })}</span> {intl.formatMessage({ id: "schoolSectionSubtitlePart2" })}
                 </p>
-                <p className='text-school'>
+                <p  dir={direction} className='text-school'>
                     {intl.formatMessage({ id: "schoolSectionDescription1" })}
                 </p>
-                <p className='text-school'>
+                <p  dir={direction} className='text-school'>
                     {intl.formatMessage({ id: "schoolSectionDescription2" })}
                 </p>
                 <Link
