0e2fe10962ce95814978f851ecd6afed38b0a1f4
hamzakebir <hamzux200@gmail.com>
2024-03-14 00:31:45 +0000

    Readme file

--- src/components/accueil/ecole.js
+++ src/components/accueil/ecole.js
@@ -1,52 +1,63 @@
-import React from 'react'
-import { StaticImage } from 'gatsby-plugin-image'
-import '../../styles/accueil-styles/ecole.css'
+import React from 'react';
+import { StaticImage } from 'gatsby-plugin-image';
+import '../../styles/accueil-styles/ecole.css';
 import { useIntl } from 'gatsby-plugin-intl';
 import { Link } from 'gatsby';
 
 export default function Ecole() {
-  const intl = useIntl();
+  const intl = useIntl(); 
 
+  const [margin,setMargin] = React.useState({})
+
+  // Change the direction of the component when the language is Arabic
   const locale = intl.locale;
   const direction = locale === 'ar' ? 'rtl' : 'ltr';
 
+  React.useEffect(() => {
+    locale === 'ar' ? setMargin({marginRight : "10%"}) : setMargin({});
+  }, [direction]);
+
   return (
-    <>
-    <section dir={direction} className="mb-40 mt-500 conatiner-for-school">
-        <div className="px-6 py-12 text-center dark:bg-neutral-900 md:px-12 lg:text-left">
-        <div className="w-100 mx-auto sm:max-w-2xl md:max-w-3xl lg:max-w-5xl xl:max-w-7xl">
-            <div className="grid items-center gap-12 lg:grid-cols-2">
-            <div className="mt-12 lg:mt-0">
-                <h1 dir={direction} style={{  fontSize: "50px",color:"#FF64B1",marginBottom:"20px"}} className=''>
-                    {intl.formatMessage({ id: "schoolSectionTitle" })}
-                </h1>
-                <p dir={direction} className='titre-two-school'>
-                    {intl.formatMessage({ id: "schoolSectionSubtitle" })} <span style={{ color : '#FF64B1' }}>{intl.formatMessage({ id: "schoolSectionHours" })}</span> {intl.formatMessage({ id: "schoolSectionSubtitlePart2" })}
-                </p>
-                <p dir={direction} className='text-school'>
-                    {intl.formatMessage({ id: "schoolSectionDescription1" })}
-                </p>
-                <p dir={direction} className='text-school'>
-                    {intl.formatMessage({ id: "schoolSectionDescription2" })}
-                </p>
-                <Link
-                    to="/"
-                    className="btn-school"
-                >
-                    {intl.formatMessage({ id: "schoolSectionButton" })}
-                </Link>
-                </div>
-            <div className="">
-                <StaticImage
-                    src="../../images/accueil-images/for-schools.png"
-                    alt="Flowbite Logo"
-                    className='image-for-school'
-                />
-            </div>
-            </div>
+    <section dir={direction} style={{ marginTop: "200px", marginBottom: "300px" }} className="bg-white dark:bg-gray-900">
+      <div className="max-w-screen-xl px-4 py-8 mx-auto lg:py-16 flex flex-col items-center lg:flex-row">
+        <div style={margin} className="lg:w-1/2 flex flex-col justify-center items-center lg:items-start lg:justify-start">
+
+          {/* Hero title 1 */}
+          <h1 dir={direction} style={{ fontSize: "50px", color: "#FF64B1", marginBottom: "20px" }} className=''>
+            {intl.formatMessage({ id: "schoolSectionTitle" })}
+          </h1>
+
+          {/* Hero Paragraph */}
+          <p dir={direction} className='titre-two-school'>
+            {intl.formatMessage({ id: "schoolSectionSubtitle" })} <span style={{ color: '#FF64B1' }}>{intl.formatMessage({ id: "schoolSectionHours" })}</span> {intl.formatMessage({ id: "schoolSectionSubtitlePart2" })}
+          </p>
+
+          {/* Les niveaux */}
+          <p dir={direction} className='text-school'>
+            {intl.formatMessage({ id: "schoolSectionDescription1" })}
+          </p>
+          <p dir={direction} className='text-school'>
+            {intl.formatMessage({ id: "schoolSectionDescription2" })}
+          </p>
+
+          {/* la button d'inscription */}
+          <Link
+            to="/"
+            className="flex justify-center btn-school"
+          >
+            {intl.formatMessage({ id: "schoolSectionButton" })}
+          </Link>
         </div>
+
+        {/* L'image de Hero */}
+        <div className="lg:w-1/2 lg:pl-12 mt-8 lg:mt-0 flex justify-center">
+          <StaticImage
+            src="../../images/accueil-images/for-schools.png"
+            alt="Hero image"
+            className="max-w-full"
+          />
         </div>
+      </div>
     </section>
-    </>
-  )
+  );
 }
