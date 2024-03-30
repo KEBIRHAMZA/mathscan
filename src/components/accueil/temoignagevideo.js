972797515e13e49d3887b6fb5bf1d6238cdaadef
hamzakebir <hamzux200@gmail.com>
2024-03-30 22:41:14 +0000

    Temoignages video

--- src/components/accueil/temoignagevideo.js
+++ src/components/accueil/temoignagevideo.js
@@ -1,69 +1,105 @@
-import React, { useState } from 'react';
+import React from 'react';
 import '../../styles/accueil-styles/temoignagevideo.css';
 
+const adultVideoUrls = [
+    "https://www.youtube.com/embed/4B4h0a36-ic?si=ooxgikt6K52FJHf-",
+    "https://www.youtube.com/embed/G90FGCnsYDo?si=WnP11jD36qKzJldQ",
+];
+
+const childrenVideoUrls = [
+    "https://www.youtube.com/embed/2VJWUARvkIg?si=PZTJ3MBXXamGPUg6",
+    "https://www.youtube.com/embed/K9dqMe3g2tE?si=YGMU2gFQ9E9tWq0U",
+    "https://www.youtube.com/embed/VCy8-y7tr9Q?si=ZSMn3vlhDk-QACA0"
+];
+
 const Temoignagevideo = () => {
-  const [count, setCount] = useState(0);
-  const videos = [
-    "https://www.youtube.com/embed/G90FGCnsYDo?controls=0",
-    "https://www.youtube.com/embed/4B4h0a36-ic",
-    "https://www.youtube.com/embed/2VJWUARvkIg",
-  ];
-  const videoCount = videos.length;
+    return (
+        <>
+            {/* --------------------------- Temoignages adultes ------------------------------ */}
+            <div style={{ height: "100%", overflowX: "hidden", textAlign: "center", marginTop: "100px" }}>
+                <div className="flex justify-center">
+                    <h1 className='text-4xl font-bold'>Temoignages adultes</h1>
+                </div>
+                <div className="csslider infinity" id="sliderAdult">
+                    {adultVideoUrls.map((url, index) => (
+                        <input key={index} type="radio" name="slidesAdult" defaultChecked={index === 0 ? "checked" : ""} id={`slidesAdult_${index + 1}`} />
+                    ))}
+                    <ul>
+                        {adultVideoUrls.map((url, index) => (
+                            <li key={index}>
+                                <iframe
+                                    title={`Adult Video ${index + 1}`}
+                                    width="100%"
+                                    height="400px"
+                                    src={url}
+                                    frameBorder={0}
+                                    allow="autoplay; encrypted-media"
+                                    allowFullScreen=""
+                                />
+                                <p />
+                            </li>
+                        ))}
+                    </ul>
+                    <div className="arrows">
+                        {adultVideoUrls.map((_, index) => (
+                            <label key={index} htmlFor={`slidesAdult_${index + 1}`} />
+                        ))}
+                        <label className="goto-first" htmlFor="slidesAdult_1" />
+                        <label className="goto-last" htmlFor={`slidesAdult_${adultVideoUrls.length}`} />
+                    </div>
+                    <div className="navigation">
+                        <div>
+                            {adultVideoUrls.map((_, index) => (
+                                <label key={index} htmlFor={`slidesAdult_${index + 1}`} />
+                            ))}
+                        </div>
+                    </div>
+                </div>
+            </div>
 
-  const showNextItem = () => {
-    setCount((prevCount) => (prevCount < videoCount - 1 ? prevCount + 1 : 0));
-  };
-
-  const showPreviousItem = () => {
-    setCount((prevCount) => (prevCount > 0 ? prevCount - 1 : videoCount - 1));
-  };
-
-  const keyPress = (e) => {
-    if (e.keyCode === 37) {
-      showPreviousItem();
-    } else if (e.keyCode === 39) {
-      showNextItem();
-    }
-  };
-
-  return (
-    <div className="container-tem-video">
-      <h2 style={{ marginBottom : "-120px" }} className="flex justify-center text-4xl font-bold">Temoignage vidéo</h2>
-      <div className="slider">
-        {videos.map((video, index) => (
-          <iframe
-            key={index}
-            className={index === count ? 'active' : 'hidden'}
-            width="560"
-            height="315"
-            src={video}
-            frameBorder="0"
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-            allowFullScreen
-            title={`Slider ${index}`}
-          ></iframe>
-        ))}
-      </div>
-      <nav className="slider-nav">
-        <ul>
-          <li className="arrow">
-            <button className="previous" onClick={showPreviousItem}>
-              <span>
-                <i className="ion-arrow-left-c">&lt;</i>
-              </span>
-            </button>
-          </li>
-          <li className="arrow">
-            <button className="next" onClick={showNextItem}>
-              <span>
-                <i className="ion-arrow-right-c">&gt;</i>
-              </span>
-            </button>
-          </li>
-        </ul>
-      </nav>
-    </div>
-  );
+            {/* --------------------------- Temoignages enfants ------------------------------ */}
+            <div style={{ height: "100%", overflowX: "hidden", textAlign: "center", marginTop: "100px" }}>
+                <div className="flex justify-center">
+                    <h1 className='text-4xl font-bold'>Temoignages enfants</h1>
+                </div>
+                <div className="csslider infinity" id="sliderChildren">
+                    {childrenVideoUrls.map((url, index) => (
+                        <input key={index} type="radio" name="slidesChildren" defaultChecked={index === 0 ? "checked" : ""} id={`slidesChildren_${index + 1}`} />
+                    ))}
+                    <ul>
+                        {childrenVideoUrls.map((url, index) => (
+                            <li key={index}>
+                                <iframe
+                                    title={`Child Video ${index + 1}`}
+                                    width="100%"
+                                    height="400px"
+                                    src={url}
+                                    frameBorder={0}
+                                    allow="autoplay; encrypted-media"
+                                    allowFullScreen=""
+                                />
+                                <p />
+                            </li>
+                        ))}
+                    </ul>
+                    <div className="arrows">
+                        {childrenVideoUrls.map((_, index) => (
+                            <label key={index} htmlFor={`slidesChildren_${index + 1}`} />
+                        ))}
+                        <label className="goto-first" htmlFor="slidesChildren_1" />
+                        <label className="goto-last" htmlFor={`slidesChildren_${childrenVideoUrls.length}`} />
+                    </div>
+                    <div className="navigation">
+                        <div>
+                            {childrenVideoUrls.map((_, index) => (
+                                <label key={index} htmlFor={`slidesChildren_${index + 1}`} />
+                            ))}
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
 };
 
 export default Temoignagevideo;
